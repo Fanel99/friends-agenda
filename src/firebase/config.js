5bd39c0cc2c89b20c20a4edb0fcadaf0fafae431
https://github.com/Fanel99 <fanelsecara@gmail.com>
2021-12-17 10:46:27 +0100

    add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import 'firebase/firestore';
+import 'firebase/auth';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBA3mPzLmuPOT9u3-j_RYhPhCuebRK6wIo',
+  authDomain: 'fanelfriends.firebaseapp.com',
+  projectId: 'fanelfriends',
+  storageBucket: 'fanelfriends.appspot.com',
+  messagingSenderId: '363656253943',
+  appId: '1:363656253943:web:b5fa8d32c360f33cdf5ef2',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectAuth, projectFirestore, timestamp };

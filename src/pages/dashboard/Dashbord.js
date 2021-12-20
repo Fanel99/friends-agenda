0ff3285d1f1f8443d08348546e8e9d8d0a8daaba
https://github.com/Fanel99 <fanelsecara@gmail.com>
2021-12-20 08:40:09 +0100

    add projectlist component & display projects in dashboard

--- src/pages/dashboard/Dashbord.js
+++ src/pages/dashboard/Dashbord.js
@@ -1,5 +1,15 @@
 import './Dashboard.css';
+import ProjectList from '../../components/ProjectList';
+import { useCollection } from '../../hooks/useCollection';
 
 export default function Dashbord() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection('projects');
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error} </p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }

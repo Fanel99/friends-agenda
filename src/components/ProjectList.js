c179d5e851ac901ecec69712b261f33deb28647c
https://github.com/Fanel99 <fanelsecara@gmail.com>
2021-12-20 12:16:44 +0100

    add projectlist component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 import Avatar from '../components/Avatar';
 
 export default function ProjectList({ projects }) {
+  console.log(projects);
+
   return (
     <div className="project-list">
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map((project) => (
-        <Link to={`/projects/${project.id} `} key={project.id}>
-          <h4> {project.name}</h4>
-          <p>Due by {project.dueDate.toDate().toDateString()} </p>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
           <div className="assigned-to">
+            <p>
+              <strong>Assigned to:</strong>
+            </p>
             <ul>
               {project.assignedUsersList.map((user) => (
                 <li key={user.photoURL}>

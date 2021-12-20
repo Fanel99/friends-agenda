2ca76ff19eca6f122c4fe561f48d1f1cb9697117
https://github.com/Fanel99 <fanelsecara@gmail.com>
2021-12-20 13:45:54 +0100

    add comment component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import Avatar from '../components/Avatar';
 
 export default function ProjectList({ projects }) {
-  console.log(projects);
-
   return (
     <div className="project-list">
       {projects.length === 0 && <p>No projects yet!</p>}
